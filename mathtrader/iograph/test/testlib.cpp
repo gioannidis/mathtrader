a5a02d738c9c56a23937c55935776de98aba5490
George Ioannidis <george.ioannidis@epfl.ch>
2018-06-17 01:28:45 +0200

    Added google test template.

---
+++ mathtrader/iograph/test/testlib.cpp
@@ -0,0 +1,36 @@
+/* This file is part of MathTrader++.
+ *
+ * Copyright (C) 2018 George Ioannidis
+ *
+ * MathTrader++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MathTrader++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MathTrader++.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <thread>	// Google Test runs on threads
+#include <vector>
+#include <iostream>
+
+#include <gtest/gtest.h>
+#include <iograph/wantparser.hpp>
+
+TEST( LibTest, BasicTest ) {
+}
+
+int main( int argc, char ** argv ) {
+
+	testing::InitGoogleTest( &argc, argv );
+	return RUN_ALL_TESTS();
+}
