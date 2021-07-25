5521548a8b719ef18601032c571b8a4d821fe887
George Ioannidis <gioannidis@users.noreply.github.com>
2021-07-25 12:21:56 +0200

    Added bash script to retrieve statistics on officialwants.

---
+++ mathtrader/parser/test_data/stats_officialwants.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# This file is part of mathtrader.
+#
+# Copyright (C) 2021 George Ioannidis
+#
+# mathtrader is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# mathtrader is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mathtrader. If not, see <http://www.gnu.org/licenses/>.
+
+# Library used to retrieve quick statistics about a OLWLG-generated
+# "officialwants" file. It is not meant to be deployed for accurate parsing, but
+# merely as a straightforward way to retrieve statistics for test development.
+# Therefore, it is intended to be run in a Linux environment
+
+# Retrieves statistics from a given official wants file.
+function GetStatsFromOfficialwantsFile {
+
+  if [[ $# -ne 1 ]]; then
+      >&2 echo "Missing filename. Usage: $0 <filename>"
+      exit 2;
+  fi
+
+  # Merely echos the file being parsed.
+  echo "-----"
+  echo "Parsing file: $1"
+
+  # Retrieves the users from the OFFICIAL-NAMES section, e.g., "(from username)".
+  echo -n "Users: "
+  pcregrep -e "\(from (.+)\)" -o1 $1 | sort | uniq | wc -l
+
+  # Retrieves the number of official items by looking up the pattern "==>".
+  echo -n "Official items: "
+  egrep -c "==>" $1
+
+  # Retrieves the number of wantlists, looking up lines beginning with a "(".
+  echo -n "Wantlists: "
+  egrep -c "^\(" $1
+
+  # Retrieves the longest line. This is a candidate for the longest wantlist.
+  echo "Longest line(s):"
+  egrep -n "^.{$(wc -L < $1)}$" $1 | cut -d ':' -f 1
+}
+
+### Main script
+
+required_binaries="egrep pcregrep"
+
+# Checks if required binaries have been installed.
+for required_binary in ${required_binaries[@]}; do
+  if ! command -v ${required_binary} &> /dev/null; then
+    >&2 echo "${required_binary} is required to run $0, but was not found."
+    exit 1
+  fi
+done
+
+# Parses each individual file.
+for officialwants_file in mathtrader/parser/test_data/*-officialwants.txt; do
+  GetStatsFromOfficialwantsFile ${officialwants_file}
+done
