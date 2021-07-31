1bc6a248d3edcfe30b9fa0b629ff9f35bdfab756
George Ioannidis <gioannidis@users.noreply.github.com>
2021-07-31 03:20:17 +0200

    Added NodeBuilder, generating nodes from offered items.

---
+++ mathtrader/network/internal/node_builder.cc
@@ -0,0 +1,77 @@
+// This file is part of mathtrader.
+//
+// Copyright (C) 2021 George Ioannidis
+//
+// mathtrader is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// mathtrader is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with mathtrader. If not, see <http://www.gnu.org/licenses/>.
+
+#include "mathtrader/network/internal/node_builder.h"
+
+#include <string>
+#include <string_view>
+
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/str_cat.h"
+#include "ortools/base/map_util.h"
+
+#include "mathtrader/common/item.pb.h"
+#include "mathtrader/common/offered_item.pb.h"
+#include "mathtrader/common/wantlist.pb.h"
+#include "mathtrader/parser/parser_result.pb.h"
+
+namespace mathtrader::network::internal {
+
+// Generates Nodes from the parser result.
+NodeBuilder::NodeContainer NodeBuilder::BuildNodes(
+    const ParserResult& parser_result) {
+  // The node container to return.
+  NodeContainer nodes;
+
+  // Verifies that no duplicate offered items are encountered.
+  absl::flat_hash_set<std::string> offered_items;
+
+  // Processes the offered item from each wantlist.
+  for (const Wantlist& wantlist : parser_result.wantlists()) {
+    const Item& item = wantlist.offered_item();
+    const std::string& id = item.id();
+
+    // Verifies that the offered item is unique.
+    gtl::InsertOrDie(&offered_items, id);
+
+    // Creates the offered/wanted nodes.
+    Node* const offered_node = nodes.Add();
+    Node* const wanted_node = nodes.Add();
+
+    // Creates unique ids.
+    offered_node->set_id(absl::StrCat(id, "+"));
+    wanted_node->set_id(absl::StrCat(id, "-"));
+
+    // Sets the item type.
+    offered_node->set_item_type(Node::kOffered);
+    wanted_node->set_item_type(Node::kWanted);
+
+    // Sets the symmetric nodes.
+    offered_node->set_symmetric_node(wanted_node->id());
+    wanted_node->set_symmetric_node(offered_node->id());
+
+    // Sets the username.
+    {
+      const std::string& username = item.GetExtension(OfferedItem::username);
+      offered_node->set_username(username);
+      wanted_node->set_username(username);
+    }
+  }
+  return nodes;
+}
+
+}  // namespace mathtrader::network::internal
---
+++ mathtrader/network/internal/node_builder.h
@@ -0,0 +1,57 @@
+// This file is part of mathtrader.
+//
+// Copyright (C) 2021 George Ioannidis
+//
+// mathtrader is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// mathtrader is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with mathtrader. If not, see <http://www.gnu.org/licenses/>.
+
+// Builds the trade network nodes.
+
+#ifndef MATHTRADER_NETWORK_INTERNAL_NODE_BUILDER_H_
+#define MATHTRADER_NETWORK_INTERNAL_NODE_BUILDER_H_
+
+#include "mathtrader/common/node.pb.h"
+#include "mathtrader/parser/parser_result.pb.h"
+
+namespace mathtrader::network::internal {
+
+// Generates Nodes from the wantlists' offered items. Each item generates two
+// nodes, representing an "offered" and a wanted item. The offered/wanted items
+// are identified by appending a '+' and '-' suffix, respectively, to the
+// original item id.
+//
+// As an example, consider an offered item with id "0001-ITEM". It generates two
+// nodes: "0001-ITEM+" (offered) and "0001-ITEM-" (wanted).
+// Usage:
+//
+//     ParserResult parser_result;
+//     // ... populates parser_result
+//     auto node = NodeBuilder::BuildNodeContainer(parser_result);
+class NodeBuilder {
+ public:
+  // Map type that indexes Nodes by Node::id.
+  using NodeContainer = google::protobuf::RepeatedField<Node>;
+
+  NodeBuilder() = default;
+
+  // Disables copy constructor and move assignment.
+  NodeBuilder(const NodeBuilder&) = delete;
+  NodeBuilder& operator=(const NodeBuilder&) = delete;
+
+  // Generates Nodes from the parser result.
+  static NodeContainer BuildNodes(const ParserResult& parser_result);
+};
+
+}  // namespace mathtrader::network::internal
+
+#endif  // MATHTRADER_NETWORK_INTERNAL_NODE_BUILDER_H_
